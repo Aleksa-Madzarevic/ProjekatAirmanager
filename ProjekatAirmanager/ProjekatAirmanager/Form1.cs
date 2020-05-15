79b21ea98d19a7adca72949b0b5d61831475a53b
Aleksa-Madzarevic <aleksama48@gmail.com>
2020-05-15 16:21:56 +0200

    15.5.2020. kraj

--- ProjekatAirmanager/ProjekatAirmanager/Form1.cs
+++ ProjekatAirmanager/ProjekatAirmanager/Form1.cs
@@ -30,17 +30,27 @@ namespace ProjekatAirmanager
         //Graphics g
         //struktura SlikaAvion
         List<Avionce> Avioni;
+        int trajanjePoteza; //u sekundama
+        enum
+        {
+            
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
            
         }
         public static void nacrtajLet(Graphics g, Tuple<double, double> A, Tuple<double, double> B, bool inc, double brzina)
         {
-            
+            Avionce avion=new Avionce();
+
+
         }
         public static void pomeriAvione()
         {
-
+            for (int i = 0; i < Avionce.capacity; i++) { 
+                
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
